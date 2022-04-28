2f0d68a20223b6af265c3c9cd838657598874250
Matthew <leonar1509@gmail.com>
2022-04-28 20:49:57 +0300

    'window'

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import pygame
+import pygame as pg
+import sys
 
+pg.init()
 
+width = 500
+height = 500
+
+clock = pg.time.Clock()
+
+screen = pg.display.set_mode((width, height))
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit()
+
+    screen.fill((0, 0, 30))
+    pg.display.update()
+    clock.tick(60)
